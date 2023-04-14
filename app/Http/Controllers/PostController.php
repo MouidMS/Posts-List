7a77b1509e5a69e4766d0e3c2fcd424be67627a0
Mouid M. Harbe <mooyd64@gmail.com>
2023-04-14 03:37:09 +0300

    update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,23 +20,33 @@ class PostController extends Controller
     }
     public function trashed()
     {
+        // Retrieve all trashed posts
         $trashedPosts = Post::onlyTrashed()->get();
+
+        // Pass the trashed posts to the view
         return view('trashed', compact('trashedPosts'));
     }
 
     public function restore($id)
     {
+        // Find the post with the given ID and restore it
         Post::withTrashed()->find($id)->restore();
+
+        // Redirect back to the previous page with a success message
         return redirect()->back()->with('success', 'Post has been restored successfully.');
     }
 
     public function forceDelete($id)
     {
+        // Find the post with the given ID (even if it's trashed)
         $post = Post::withTrashed()->findOrFail($id);
+
+        // Permanently delete the post
         $post->forceDelete();
+
+        // Redirect to the posts index page with a success message
         return redirect()->route('posts.index')->with('success','Post permanently deleted successfully!');
     }
-
 
 
     public function create()

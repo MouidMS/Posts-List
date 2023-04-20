cafb91eb3cb729c94ae45828d553b1aaa6a50f2b
Mouid M. Harbe <mooyd64@gmail.com>
2023-04-20 04:32:06 +0300

    Add test for Post

--- database/migrations/2023_04_13_212136_create_posts_table.php
+++ database/migrations/2023_04_13_212136_create_posts_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -13,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->uuid('uuid');
+            $table->uuid('uuid')->default(Str::uuid())->unique();
             $table->string('title');
             $table->text('body');
             $table->unsignedBigInteger('user_id'); // add user_id column
--- tests/Feature/PostsTest.php
+++ tests/Feature/PostsTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class PostsTest extends TestCase
@@ -11,10 +14,57 @@ class PostsTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function testCreateNewUserAndLogin(): void
     {
-        $response = $this->get('/');
+        $email = 'johndoe' . rand(1, 1000) . '@example.com';
 
-        $response->assertStatus(302);
+        $user = User::create([
+            'name' => 'John Doe',
+            'email' => $email,
+            'password' => bcrypt('password'),
+        ]);
+
+        // Log the user in
+        $this->actingAs($user);
+        // Assert that the user is authenticated
+        $this->assertAuthenticated();
+
+        // Add additional assertions as needed
+
+        return;
+    }
+
+    public function testCreatePost(): void
+    {
+        // Call the testCreateNewUserAndLogin method to log the user in
+        $this->testCreateNewUserAndLogin();
+
+        // Use the ID of the logged-in user instead of hardcoding the value
+        $user_id = auth()->user()->id;
+
+        $uuid = \Illuminate\Support\Str::uuid();
+        Post::create([
+            'title' => 'fdf',
+            'body' => 'tset',
+            'user_id' => $user_id,
+            'uuid' => $uuid,
+        ]);
+
+        $response = $this->get('/posts');
+
+        $response->assertStatus(200);
+    }
+
+    public function testDeleteAllUsersAndPosts(): void
+    {
+        // Delete all posts
+        Post::query()->delete();
+
+        // Delete all users
+        User::query()->delete();
+
+        // Check if the tables are empty
+        $this->assertEquals(0, User::count());
+        $this->assertEquals(0, Post::count());
     }
 }
